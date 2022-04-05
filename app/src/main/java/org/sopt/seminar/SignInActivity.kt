ddbb85b379cda1fef17add9bb700032576bdae38
kimsubeen <papajj06@naver.com>
2022-04-06 02:21:49 +0900

    [feat] signin, signup view 구현

---
+++ app/src/main/java/org/sopt/seminar/SignInActivity.kt
@@ -0,0 +1,11 @@
+package org.sopt.seminar
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
--- app/src/main/java/org/sopt/seminar/SignUpActivity.kt
+++ app/src/main/java/org/sopt/seminar/SignUpActivity.kt
@@ -3,9 +3,9 @@ package org.sopt.seminar
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
     }
 }
