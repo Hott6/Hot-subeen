4292018829406d3e1ac100bc6c8d9dc54a18e48f
kimsubeen <papajj06@naver.com>
2022-04-07 03:59:44 +0900

    [feat] DataBinding으로 변경
    viewBinding->dataBinding

--- app/src/main/java/org/sopt/seminar/HomeActivity.kt
+++ app/src/main/java/org/sopt/seminar/HomeActivity.kt
@@ -2,15 +2,14 @@ package org.sopt.seminar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import org.sopt.seminar.databinding.ActivityHomeBinding
-import org.sopt.seminar.databinding.ActivitySignInBinding
 
 class HomeActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityHomeBinding
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityHomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+        binding.user = User("김수빈", "23", "ENFJ")
     }
 }
