04f5ecdf7daeef1ffcd6e38a9bb1974a7c6b3d23
kimsubeen <papajj06@naver.com>
2022-04-14 13:25:29 +0900

    [FEAT] HomeActivity fragment 구현

--- app/src/main/java/org/sopt/seminar/HomeActivity.kt
+++ app/src/main/java/org/sopt/seminar/HomeActivity.kt
@@ -6,10 +6,39 @@ import androidx.databinding.DataBindingUtil
 import org.sopt.seminar.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private var position = FOLLOWER_POSITION
     private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
         binding.user = User("김수빈", "23", "ENFJ")
+
+        inintTransactionEvent()
+    }
+
+    private fun inintTransactionEvent() {
+        val followerFragment = FollowerFragment()
+        val repoFragment = RepoFragment()
+
+        supportFragmentManager.beginTransaction().add(R.id.fragment_home, followerFragment).commit()
+        binding.btnFollower.setOnClickListener {
+            val transaction = supportFragmentManager.beginTransaction()
+            when (position) {
+                FOLLOWER_POSITION -> {
+                    transaction.replace(R.id.fragment_home, repoFragment)
+                    position = REPO_POSITION
+                }
+                REPO_POSITION -> {
+                    transaction.replace(R.id.fragment_home, followerFragment)
+                    position = FOLLOWER_POSITION
+                }
+            }
+            transaction.commit()
+        }
+    }
+
+    companion object {
+        const val FOLLOWER_POSITION = 1
+        const val REPO_POSITION = 2
     }
 }
