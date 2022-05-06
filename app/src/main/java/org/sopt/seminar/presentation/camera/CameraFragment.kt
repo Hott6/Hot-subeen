a830ce7620211a72863c5d434c3880c5e0371e44
kimsubeen <papajj06@naver.com>
2022-05-06 14:33:38 +0900

    [FEAT] 갤러리 이미지 가져오기

--- app/src/main/java/org/sopt/seminar/presentation/camera/CameraFragment.kt
+++ app/src/main/java/org/sopt/seminar/presentation/camera/CameraFragment.kt
@@ -1,19 +1,64 @@
 package org.sopt.seminar.presentation.camera
 
+import android.app.Activity.RESULT_CANCELED
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.Glide
 import org.sopt.seminar.R
+import org.sopt.seminar.databinding.FragmentCameraBinding
+import org.sopt.seminar.showToast
 
 class CameraFragment : Fragment() {
-
+    private var _binding: FragmentCameraBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false)
+        _binding = FragmentCameraBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnClickEvent()
+    }
+
+    private fun btnClickEvent() {
+        binding.btnAddImage.setOnClickListener {
+            goGallery()
+        }
+    }
+
+    private val activityLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK && it.data != null) {
+                var currentImageUri = it.data?.data
+                Glide.with(requireActivity()).load(currentImageUri).into(binding.ivGalleryImage)
+
+            } else if (it.resultCode == RESULT_CANCELED) {
+                requireActivity().showToast("사진 선택 취소")
+            } else {
+                requireActivity().showToast("사진 첨부 실패")
+            }
+        }
+
+    private fun goGallery() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        activityLauncher.launch(intent)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
