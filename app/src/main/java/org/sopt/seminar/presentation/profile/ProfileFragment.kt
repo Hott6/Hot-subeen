e5c97a77b4663cdaf12da9aa2151de69b030de2d
kimsubeen <papajj06@naver.com>
2022-05-11 15:37:33 +0900

    [CHORE] coroutine build 설정

--- app/src/main/java/org/sopt/seminar/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/org/sopt/seminar/presentation/profile/ProfileFragment.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentTransaction
 import com.bumptech.glide.Glide
